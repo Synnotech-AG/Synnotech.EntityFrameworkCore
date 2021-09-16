d01d46d609c09ca57181843b5ba2d0f4f5a09e44
Kenny Pflug <kenny.pflug@live.de>
2021-09-16 16:59:39 +0200

    added helper methods for composition root

---
+++ Code/src/Synnotech.EntityFrameworkCore.MsSqlServer/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Light.GuardClauses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Synnotech.EntityFrameworkCore.MsSqlServer
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEntityFrameworkCoreForMsSqlServer<TDbContext>(this IServiceCollection services,
+                                                                                          string configurationSectionName = EfCoreSettings.DefaultSectionName,
+                                                                                          ServiceLifetime dbContextLifetime = ServiceLifetime.Transient,
+                                                                                          Action<SqlServerDbContextOptionsBuilder>? configureSqlServer = null,
+                                                                                          Action<DbContextOptionsBuilder<TDbContext>>? configureOptions = null)
+            where TDbContext : DbContext
+        {
+            services.MustNotBeNull(nameof(services));
+
+            services.AddSingleton(container => EfCoreSettings.FromConfiguration(container.GetRequiredService<IConfiguration>(), configurationSectionName))
+                    .AddSingleton(container =>
+                     {
+                         var settings = container.GetRequiredService<EfCoreSettings>();
+                         var builder = new DbContextOptionsBuilder<TDbContext>().UseSqlServer(settings.ConnectionString, configureSqlServer)
+                                                                                .EnableLoggingIfNecessary(container, settings.LoggingBehavior);
+                         configureOptions?.Invoke(builder);
+                         return builder.Options;
+                     })
+                    .AddDbContext<TDbContext>(dbContextLifetime);
+
+            return services;
+        }
+    }
+}
---
+++ Code/src/Synnotech.EntityFrameworkCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Light.GuardClauses;
+using Light.GuardClauses.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Synnotech.EntityFrameworkCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDbContext<TDbContext>(this IServiceCollection services, ServiceLifetime dbContextLifetime = ServiceLifetime.Transient)
+            where TDbContext : DbContext
+        {
+            services.MustNotBeNull(nameof(services));
+            services.Add(new ServiceDescriptor(typeof(TDbContext), typeof(TDbContext), dbContextLifetime));
+            return services;
+        }
+
+        public static DbContextOptionsBuilder<TDbContext> EnableLoggingIfNecessary<TDbContext>(this DbContextOptionsBuilder<TDbContext> builder, IServiceProvider container, LoggingBehavior behavior)
+            where TDbContext : DbContext
+        {
+            builder.MustNotBeNull(nameof(builder));
+            container.MustNotBeNull(nameof(container));
+
+            if (behavior == LoggingBehavior.Off)
+                return builder;
+
+            var loggerFactory = container.GetService<ILoggerFactory>();
+            if (loggerFactory == null)
+                throw new InvalidConfigurationException($"The logging behavior for EF Core is set to \"{behavior}\", but there is no ILoggerFactory registered with the DI container.");
+
+            builder.UseLoggerFactory(loggerFactory);
+            if (behavior == LoggingBehavior.EnabledWithSensitiveData)
+                builder.EnableSensitiveDataLogging();
+
+            return builder;
+        }
+    }
+}
