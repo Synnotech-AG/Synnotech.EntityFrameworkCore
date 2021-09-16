aa7929893b69e505f1bf3770e7aaa66c1863df27
Kenny Pflug <kenny.pflug@live.de>
2021-09-16 13:00:37 +0200

    added in-memory test for AsyncReadOnlySession

--- Code/Synnotech.EntityFrameworkCore.Tests/AsyncReadOnlySessionTests.cs
+++ Code/Synnotech.EntityFrameworkCore.Tests/AsyncReadOnlySessionTests.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Synnotech.DatabaseAbstractions;
 using Xunit;
 
@@ -7,9 +10,42 @@ namespace Synnotech.EntityFrameworkCore.Tests
     public static class AsyncReadOnlySessionTests
     {
         [Fact]
-        public static void MustImplementIAsyncReadOnlySession()
-        {
+        public static void MustImplementIAsyncReadOnlySession() =>
             typeof(AsyncReadOnlySession<>).Should().Implement<IAsyncReadOnlySession>();
+
+        [Fact]
+        public static async Task LoadData()
+        {
+            var contacts = new Contact[]
+            {
+                new () { Id = 1, Name = "John Doe" },
+                new () { Id = 2, Name = "Margaret Jones" }
+            };
+            var context = TestContext.CreateInMemory();
+            await context.Contacts.AddRangeAsync(contacts);
+            await context.SaveChangesAsync();
+
+            List<Contact> loadedContacts;
+            await using (var session = new EfGetContactsSession(context))
+            {
+                loadedContacts = await session.GetContactsAsync();
+            }
+
+            loadedContacts.Should().Equal(contacts);
+            context.MustHaveBeenDisposed();
+        }
+
+        private interface IGetContactsSession : IAsyncReadOnlySession
+        {
+            Task<List<Contact>> GetContactsAsync();
+        }
+
+        private sealed class EfGetContactsSession : AsyncReadOnlySession<TestContext>, IGetContactsSession
+        {
+            public EfGetContactsSession(TestContext context) : base(context) { }
+
+            public Task<List<Contact>> GetContactsAsync() =>
+                Context.Contacts.ToListAsync();
         }
     }
 }
---
+++ Code/Synnotech.EntityFrameworkCore.Tests/TestDataAccess.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Light.GuardClauses;
+using Microsoft.EntityFrameworkCore;
+
+namespace Synnotech.EntityFrameworkCore.Tests
+{
+    public class TestContext : DbContext
+    {
+        public TestContext(DbContextOptions<TestContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts => Set<Contact>();
+
+        public int DisposeCallCount { get; private set; }
+
+        public int SaveChangesCallCount { get; private set; }
+
+        public static TestContext CreateInMemory([CallerMemberName] string? databaseName = null)
+        {
+            databaseName.MustNotBeNull(nameof(databaseName));
+
+            var options = new DbContextOptionsBuilder<TestContext>().UseInMemoryDatabase(databaseName!)
+                                                                    .Options;
+            return new TestContext(options);
+        }
+
+        public override void Dispose()
+        {
+            DisposeCallCount++;
+            base.Dispose();
+        }
+
+        public override ValueTask DisposeAsync()
+        {
+            DisposeCallCount++;
+            return base.DisposeAsync();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            SaveChangesCallCount++;
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            SaveChangesCallCount++;
+            return base.SaveChanges();
+        }
+
+        public TestContext SaveChangesMustHaveBeenCalled()
+        {
+            SaveChangesCallCount.Should().Be(1);
+            return this;
+        }
+
+        public TestContext SaveChangesMustNotHaveBeenCalled()
+        {
+            SaveChangesCallCount.Should().Be(0);
+            return this;
+        }
+
+        public TestContext MustHaveBeenDisposed()
+        {
+            DisposeCallCount.Should().BeGreaterThan(0);
+            return this;
+        }
+    }
+
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
