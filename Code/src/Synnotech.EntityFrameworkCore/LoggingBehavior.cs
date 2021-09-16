02c630a9d29780911c56bda852f7e975da73d1d6
Kenny Pflug <kenny.pflug@live.de>
2021-09-16 16:22:05 +0200

    added EF Core settings

---
+++ Code/src/Synnotech.EntityFrameworkCore/LoggingBehavior.cs
@@ -0,0 +1,23 @@
+﻿namespace Synnotech.EntityFrameworkCore
+{
+    /// <summary>
+    /// Represents the different Entity Framework Core logging modes.
+    /// </summary>
+    public enum LoggingBehavior
+    {
+        /// <summary>
+        /// Indicates that EF logging is turned off.
+        /// </summary>
+        Off = 0,
+
+        /// <summary>
+        /// Indicates that logging is enabled.
+        /// </summary>
+        Enabled = 1,
+
+        /// <summary>
+        /// Indicates that logging is enabled and parameters are included.
+        /// </summary>
+        EnabledWithSensitiveData = 2
+    }
+}
