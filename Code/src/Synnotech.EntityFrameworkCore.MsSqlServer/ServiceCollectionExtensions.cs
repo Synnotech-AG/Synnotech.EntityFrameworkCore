43bbf028ed87fdadc5963285452b6757dc940242
Kenny Pflug <kenny.pflug@live.de>
2021-09-17 09:23:45 +0200

    added XML comments for ServiceCollectionExtensions

--- Code/src/Synnotech.EntityFrameworkCore.MsSqlServer/ServiceCollectionExtensions.cs
+++ Code/src/Synnotech.EntityFrameworkCore.MsSqlServer/ServiceCollectionExtensions.cs
@@ -7,8 +7,41 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Synnotech.EntityFrameworkCore.MsSqlServer
 {
+    /// <summary>
+    /// Provides extensions methods to register Entity Framework Core for MS SQL server with the DI container
+    /// and integrating with the <see cref="IConfiguration" /> infrastructure.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// <para>
+        /// Registers Entity Framework Core with the DI container. This is done by loading <see cref="EfCoreSettings" />
+        /// from the <see cref="IConfiguration" /> (that must already be registered with the container) and register
+        /// these settings as a singleton. Also, DbContextOptions&lt;TDbContext&gt; is registered as a singleton and
+        /// configured via the <see cref="EfCoreSettings" /> (connection string and logging). Finally, your <typeparamref name="TDbContext" />
+        /// is registered with the DI container, by default using a transient lifetime.
+        /// </para>
+        /// <para>
+        /// You can customize this registration process by providing a different <paramref name="dbContextLifetime" />, or by providing the
+        /// <paramref name="configureSqlServer" /> and <paramref name="configureOptions" /> delegates where you can manually adjust the SQL server
+        /// options and the DB context options.
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TDbContext">Your subtype that derives from <see cref="DbContext" />.</typeparam>
+        /// <param name="services">The collection that holds all registrations for the DI container.</param>
+        /// <param name="configurationSectionName">
+        /// The name of the configuration section that will be deserialized as <see cref="EfCoreSettings" /> (optional).
+        /// The default value is "database".
+        /// </param>
+        /// <param name="dbContextLifetime">
+        /// The lifetime the <typeparamref name="TDbContext" /> is registered with the DI container (optional).
+        /// The default value is transient. In general, we recommend that an orchestrator (such as a controller) is
+        /// responsible for opening and closing a database session, instead of relying on the DI container doing that
+        /// implicitly. If you want to stray from this principle, you can provide <see cref="ServiceLifetime.Scoped" />.
+        /// </param>
+        /// <param name="configureSqlServer">The delegate that further configures the SQL server access (optional).</param>
+        /// <param name="configureOptions">The delegate that further configures the DB context options.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null.</exception>
         public static IServiceCollection AddEntityFrameworkCoreForMsSqlServer<TDbContext>(this IServiceCollection services,
                                                                                           string configurationSectionName = EfCoreSettings.DefaultSectionName,
                                                                                           ServiceLifetime dbContextLifetime = ServiceLifetime.Transient,
--- Code/src/Synnotech.EntityFrameworkCore/ServiceCollectionExtensions.cs
+++ Code/src/Synnotech.EntityFrameworkCore/ServiceCollectionExtensions.cs
@@ -7,17 +7,40 @@ using Microsoft.Extensions.Logging;
 
 namespace Synnotech.EntityFrameworkCore
 {
+    /// <summary>
+    /// Provides extension methods for registering Entity Framework Core with the DI container.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddDbContext<TDbContext>(this IServiceCollection services, ServiceLifetime dbContextLifetime = ServiceLifetime.Transient)
+        /// <summary>
+        /// Registers your DB context with the DI container.
+        /// </summary>
+        /// <typeparam name="TDbContext">Your subtype that derives from <see cref="DbContext" />.</typeparam>
+        /// <param name="services">The collection that holds all registrations for the DI container.</param>
+        /// <param name="dbContextLifetime">
+        /// The lifetime of the DB context (optional). The default value is <see cref="ServiceLifetime.Transient" />.
+        /// </param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="services" /> is null.</exception>
+        public static IServiceCollection AddDbContext<TDbContext>(this IServiceCollection services,
+                                                                  ServiceLifetime dbContextLifetime = ServiceLifetime.Transient)
             where TDbContext : DbContext
         {
-            services.MustNotBeNull(nameof(services));
-            services.Add(new ServiceDescriptor(typeof(TDbContext), typeof(TDbContext), dbContextLifetime));
+            services.MustNotBeNull(nameof(services))
+                    .Add(new ServiceDescriptor(typeof(TDbContext), typeof(TDbContext), dbContextLifetime));
             return services;
         }
 
-        public static DbContextOptionsBuilder<TDbContext> EnableLoggingIfNecessary<TDbContext>(this DbContextOptionsBuilder<TDbContext> builder, IServiceProvider container, LoggingBehavior behavior)
+        /// <summary>
+        /// Enables Entity Framework Core logging based on the specified logging behavior.
+        /// </summary>
+        /// <typeparam name="TDbContext">Your subtype that derives from <see cref="DbContext" />.</typeparam>
+        /// <param name="builder">The options builder for the DB context.</param>
+        /// <param name="container">The DI container that is used to resolve an <see cref="ILoggerFactory" /> when logging is enabled.</param>
+        /// <param name="behavior">The value indicating whether logging is enabled.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder" /> or <paramref name="container" /> is null.</exception>
+        public static DbContextOptionsBuilder<TDbContext> EnableLoggingIfNecessary<TDbContext>(this DbContextOptionsBuilder<TDbContext> builder,
+                                                                                               IServiceProvider container,
+                                                                                               LoggingBehavior behavior)
             where TDbContext : DbContext
         {
             builder.MustNotBeNull(nameof(builder));
